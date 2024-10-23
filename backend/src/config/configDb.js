1bd4f4d96d3b3897bacabd1afd76bbcc72515e25
Marco <63360454+marc0cl@users.noreply.github.com>
2024-10-22 23:01:11 -0300

    Fixed connection to PosteSql (#1)
    * Feature: Fixed connection a PostreSql
    
    * Feature: ignored idea
    
    * Feature: ignored idea
    
    * Feature: ignored idea

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -1,18 +1,21 @@
 "use strict";
 import { DataSource } from "typeorm";
-import { DATABASE, DB_USERNAME, HOST, PASSWORD } from "./configEnv.js";
+import { DATABASE, DB_USERNAME, HOST, PASSWORD, PORT } from "./configEnv.js";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: `${HOST}`,
-  port: 5432,
+  port: `${PORT}`,
   username: `${DB_USERNAME}`,
   password: `${PASSWORD}`,
   database: `${DATABASE}`,
   entities: ["src/entity/**/*.js"],
   synchronize: true,
   logging: false,
-});
+  ssl: {
+      rejectUnauthorized: false,
+    }
+  });
 
 export async function connectDB() {
   try {

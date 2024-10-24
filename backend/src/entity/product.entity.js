36a953e3f82e6502eb4c546ddf983052ffc7bce1
Francisco Javier Cisterna Busto <francisco.cisterna2101@alumnos.ubiobio.cl>
2024-10-24 13:40:35 -0300

    se realizan la creacion de los objetos en este caso los productos con sus respectivas caracteristicas

---
+++ backend/src/entity/product.entity.js
@@ -0,0 +1,69 @@
+"use strict"
+import { EntitySchema } from "typeorm";
+
+const ProductSchema = new EntitySchema({
+  name: "Product",
+  tableName: "products",
+  columns: {
+    id: {
+        type: "int",
+        primary: true,
+        generated: true,
+    },
+    
+    codigoIdentidicador: {
+        type: "varchar",
+        length: 20,
+        nullable: false,
+        unique: true,
+    },
+
+    nombreProducto:{
+        type: "varchar",
+        length: 255,
+        nullable: false,
+
+    },
+
+    cantidadProducto:{
+        type: "int",
+        nullable: false,
+    },
+
+    fechaDeCaducidad:{
+        type: "date",
+        nullable: false,
+    },
+
+    tipoDeProducto:{
+        type: "varchar",
+        length: 255,
+        nullable: false 
+    },
+    
+    createdAt:{
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        nullable: false,
+    },
+    
+    updatedAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        onUpdate: "CURRENT_TIMESTAMP",
+        nullable: false,
+      },
+  },
+  indices: [
+    {
+        name: "IDX_PRODUCT_ID",
+        columns: ["id"],
+        unique:true,
+    },
+    {
+        name: "IDX_PRODUCT_CODIGO",
+        columns: ["codigoIdentidicador"],
+        unique:true
+    },
+  ],  
+});

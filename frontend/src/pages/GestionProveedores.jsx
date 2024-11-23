23acaa07393287bbe19c7a563b4a9d9be4010290
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2024-11-23 20:32:29 -0300

    ADD Table gestprov

--- frontend/src/pages/GestionProveedores.jsx
+++ frontend/src/pages/GestionProveedores.jsx
@@ -1,18 +1,46 @@
-import React from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState} from "react";
 import '../styles/GestionProveedores.css';
+import Table from '../components/Table';
 
 const GestionProveedores = () => {
-  const navigate = useNavigate();
+  const [proveedores, setProveedores] = useState([]);
+
+  useEffect(() => {
+    const fetchProveedores = async () => {
+      try {
+        const response = await fetch('http://localhost:24338/api/prov/all/t');
+        const data = await response.json();
+        console.log(data); // Verifica los datos recibidos
+        setProveedores(data.data);
+      } catch (error) {
+        console.error('Error fetching proveedores:', error);
+      }
+    };
+
+    fetchProveedores();
+  }, []);
 
   const handleRegisterClick = () => {
   };
 
-    return (
-      <div className="gestion-proveedores-container">
-        <button className="register-button" onClick={handleRegisterClick}>Registrar Proveedor</button>
-      </div>
-    );
+  const columns = [
+    { title: 'ID', field: 'id' },
+    { title: 'Nombre', field: 'nombre' },
+    { title: 'Dirección', field: 'direccion' },
+    { title: 'Teléfono', field: 'telefono' },
+    { title: 'Email', field: 'email' },
+    { title: 'Medio de Pago', field: 'medioPago' },
+    { title: 'Productos', field: 'productos' }
+  ];
+
+
+  return (
+    <div className="gestion-proveedores-container">
+      <h1>Proveedores</h1>
+      <button className="register-button" onClick={handleRegisterClick}>Registrar Proveedor</button>
+      <Table columns={columns} data={proveedores} />
+    </div>
+  );
 };
 
 export default GestionProveedores;

da7ac8e665c4b02735e2d21742199c1124e148d5
Francisco Javier Cisterna Busto <francisco.cisterna2101@alumnos.ubiobio.cl>
2024-11-25 20:00:46 -0300

    se hacen las validations apartes para el update

--- backend/src/validations/product.validation.js
+++ backend/src/validations/product.validation.js
@@ -80,3 +80,46 @@ export const productBodyValidation = Joi.object({
       "object.missing": "Debes proporcionar al menos un parámetro: id o codigoIdentificador.",
     });
   
+    export const productBodyUpdateValidation = Joi.object({
+      nombreProducto: Joi.string()
+        .min(3)
+        .max(255)
+        .required()
+        .messages({
+          "string.base": "El nombre del producto debe ser de tipo string.",
+          "string.empty": "El nombre del producto no puede estar vacío.",
+          "string.min": "El nombre del producto debe tener al menos 3 caracteres.",
+          "string.max": "El nombre del producto no puede exceder los 255 caracteres.",
+        }),
+      cantidadProducto: Joi.number()
+        .integer()
+        .positive()
+        .required()
+        .messages({
+          "number.base": "La cantidad del producto debe ser un número.",
+          "number.integer": "La cantidad del producto debe ser un número entero.",
+          "number.positive": "La cantidad del producto debe ser un número positivo.",
+        }),
+      fechaDeCaducidad: Joi.date()
+        .required()
+        .messages({
+          "date.base": "La fecha de caducidad debe ser una fecha válida.",
+          "any.required": "La fecha de caducidad es obligatoria.",
+        }),
+      tipoDeProducto: Joi.string()
+        .min(3)
+        .max(255)
+        .required()
+        .messages({
+          "string.base": "El tipo de producto debe ser de tipo string.",
+          "string.empty": "El tipo de producto no puede estar vacío.",
+          "string.min": "El tipo de producto debe tener al menos 3 caracteres.",
+          "string.max": "El tipo de producto no puede exceder los 255 caracteres.",
+        }),
+    
+    })
+      .unknown(false)
+      .messages({
+        "object.unknown": "No se permiten propiedades adicionales.",
+        "object.missing": "Debe proporcionar al menos un campo válido.",
+      });

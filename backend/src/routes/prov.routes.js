da57e3dab75d10e9d5d6e56e300be497de195703
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2024-11-27 21:15:57 -0300

    Update prov.routes

--- backend/src/routes/prov.routes.js
+++ backend/src/routes/prov.routes.js
@@ -1,13 +1,17 @@
 "use strict";
 import { Router } from "express";
 import { createProv, getProv, getProvs, updateProv, deleteProv } from '../controllers/prov.controller.js';
+import { isAdmin } from '../middlewares/authorization.middleware.js';
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 
 const router = Router();
 
-router.post('/', createProv);
-router.get('/:id', getProv);
-router.get('/all/t', getProvs);
-router.put('/:id', updateProv);
-router.delete('/:id', deleteProv);
+router.use(authenticateJwt);
+
+router.post('/', isAdmin, createProv);
+router.get('/:id', isAdmin, getProv);
+router.get('/all/t', isAdmin, getProvs);
+router.put('/:id', isAdmin, updateProv);
+router.delete('/:id', isAdmin, deleteProv);
 
 export default router;

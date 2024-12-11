4d6376eeed92c92c25c7226012c86f9283de21c7
Francisco Javier Cisterna Busto <francisco.cisterna2101@alumnos.ubiobio.cl>
2024-12-11 14:35:49 -0300

    se hacen cambio a las validaciones

--- backend/src/validations/product.validation.js
+++ backend/src/validations/product.validation.js
@@ -85,46 +85,57 @@ export const productBodyValidation = Joi.object({
         "object.missing": "Debes proporcionar al menos un parámetro: id, codigoIdentificador o nombreProducto.",
       });
   
-    export const productBodyUpdateValidation = Joi.object({
-      nombreProducto: Joi.string()
-        .min(3)
-        .max(255)
-        .required()
+      export const productBodyUpdateValidation = Joi.object({
+        stock: Joi.number()
+          .integer()
+          .positive()
+          .required()
+          .messages({
+            "number.base": "El stock debe ser un número.",
+            "number.integer": "El stock debe ser un número entero.",
+            "number.positive": "El stock debe ser un número positivo.",
+          })
+          .when('cantidadProducto', {
+            is: Joi.exist(), // Si cantidadProducto existe en el body
+            then: Joi.number().max(Joi.ref('cantidadProducto')).messages({
+              'number.max': 'El stock no puede ser mayor que la cantidad del producto.'
+            }),
+            otherwise: Joi.number().max(1000).messages({
+              'number.max': 'El stock no puede ser mayor a 1000.'
+            }) // Si cantidadProducto no existe, puedes aplicar otra validación, si es necesario
+          }),
+      
+        cantidadProducto: Joi.number()
+          .integer()
+          .positive()
+          .optional() // Ya no es requerido
+          .messages({
+            "number.base": "La cantidad del producto debe ser un número.",
+            "number.integer": "La cantidad del producto debe ser un número entero.",
+            "number.positive": "La cantidad del producto debe ser un número positivo.",
+          }),
+      
+        fechaDeCaducidad: Joi.date()
+          .optional()
+          .messages({
+            "date.base": "La fecha de caducidad debe ser una fecha válida.",
+            "any.required": "La fecha de caducidad es obligatoria.",
+          }),
+      
+        tipoDeProducto: Joi.string()
+          .min(3)
+          .max(255)
+          .optional()
+          .messages({
+            "string.base": "El tipo de producto debe ser de tipo string.",
+            "string.empty": "El tipo de producto no puede estar vacío.",
+            "string.min": "El tipo de producto debe tener al menos 3 caracteres.",
+            "string.max": "El tipo de producto no puede exceder los 255 caracteres.",
+          }),
+      
+      })
+        .unknown(false)
         .messages({
-          "string.base": "El nombre del producto debe ser de tipo string.",
-          "string.empty": "El nombre del producto no puede estar vacío.",
-          "string.min": "El nombre del producto debe tener al menos 3 caracteres.",
-          "string.max": "El nombre del producto no puede exceder los 255 caracteres.",
-        }),
-      cantidadProducto: Joi.number()
-        .integer()
-        .positive()
-        .required()
-        .messages({
-          "number.base": "La cantidad del producto debe ser un número.",
-          "number.integer": "La cantidad del producto debe ser un número entero.",
-          "number.positive": "La cantidad del producto debe ser un número positivo.",
-        }),
-      fechaDeCaducidad: Joi.date()
-        .required()
-        .messages({
-          "date.base": "La fecha de caducidad debe ser una fecha válida.",
-          "any.required": "La fecha de caducidad es obligatoria.",
-        }),
-      tipoDeProducto: Joi.string()
-        .min(3)
-        .max(255)
-        .required()
-        .messages({
-          "string.base": "El tipo de producto debe ser de tipo string.",
-          "string.empty": "El tipo de producto no puede estar vacío.",
-          "string.min": "El tipo de producto debe tener al menos 3 caracteres.",
-          "string.max": "El tipo de producto no puede exceder los 255 caracteres.",
-        }),
-    
-    })
-      .unknown(false)
-      .messages({
-        "object.unknown": "No se permiten propiedades adicionales.",
-        "object.missing": "Debe proporcionar al menos un campo válido.",
-      });
+          "object.unknown": "No se permiten propiedades adicionales.",
+          "object.missing": "Debe proporcionar al menos un campo válido.",
+        });
